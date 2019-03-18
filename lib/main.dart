89fc8540e9382fc050a6732f549b6cff13abb217
J2kwes <31591236+J2kwes@users.noreply.github.com>
2019-03-18 14:51:49 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.grey,
-        accentColor: Colors.amber[900]
-      ),
+          brightness: Brightness.dark,
+          primarySwatch: Colors.grey,
+          accentColor: Colors.amber[900]),
       home: Scaffold(
         appBar: new AppBar(
           // Title
@@ -21,32 +20,40 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  // ******************** Hier word de code uitgevoerd zodra de foto word aangeraakt ********************
+                  _debugString = 'onTap werkt!!!!!';
+                  print(_debugString);
+                },
+                child: Image.asset('images/shhh.png'),
+              ),
               ButtonBar(
                 alignment: MainAxisAlignment.center,
                 children: <Widget>[
                   RaisedButton(
-                    child: Text('Button'),
+                    child: Text('5 MIN'),
                     onPressed: () => {
                           _debugString = 'Knop 1 Werkt',
                           debugPrint(_debugString),
                         },
                   ),
                   RaisedButton(
-                    child: Text('Button'),
+                    child: Text('10 MIN'),
                     onPressed: () => {
                           _debugString = 'Knop 2 Werkt',
                           debugPrint(_debugString),
                         },
                   ),
                   RaisedButton(
-                    child: Text('Button'),
+                    child: Text('15 MIN'),
                     onPressed: () => {
                           _debugString = 'Knop 3 Werkt',
                           debugPrint(_debugString),
                         },
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
