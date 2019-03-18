5578865ae34a5ed74322903f4869eb33defab5d6
J2kwes <31591236+J2kwes@users.noreply.github.com>
2019-03-18 14:28:46 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.grey,
+        accentColor: Colors.amber[900]
+      ),
       home: Scaffold(
         appBar: new AppBar(
           // Title
           title: new Text("Baby App"),
         ),
         body: Center(
-          child: Column(children: <Widget>[
-            RaisedButton(
-              child: Text('Button'),
-              onPressed: () => {
-                _debugString = 'Knop Werkt',
-                debugPrint (_debugString)
-              },
-            )
-          ]),
+          child: Column(
+            children: <Widget>[
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Button'),
+                    onPressed: () => {
+                          _debugString = 'Knop 1 Werkt',
+                          debugPrint(_debugString),
+                        },
+                  ),
+                  RaisedButton(
+                    child: Text('Button'),
+                    onPressed: () => {
+                          _debugString = 'Knop 2 Werkt',
+                          debugPrint(_debugString),
+                        },
+                  ),
+                  RaisedButton(
+                    child: Text('Button'),
+                    onPressed: () => {
+                          _debugString = 'Knop 3 Werkt',
+                          debugPrint(_debugString),
+                        },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
