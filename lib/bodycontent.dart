8287dc246548d70539e10acff020ccc4d58dfd93
J2kwes <31591236+J2kwes@users.noreply.github.com>
2019-03-26 15:32:03 +0100

    individual timers added

--- lib/bodycontent.dart
+++ lib/bodycontent.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import './gesturedetector.dart';
 
 class BodyContent extends StatefulWidget {
-  int getal=3;
+  int getal = 3;
   BodyContent();
   @override
   State<StatefulWidget> createState() {
@@ -16,22 +16,66 @@ class BodyContent extends StatefulWidget {
 class _bodyState extends State<BodyContent> {
   Timer _timer;
   int _start = 5;
+  int _start2 = 10;
+  int _start3 = 15;
 
   bool isTimer = false;
 
   void startTimer() {
-    const oneMin = const Duration(minutes: 1);
+    const oneSec = const Duration(seconds: 1);
     _timer = new Timer.periodic(
-        oneMin,
-        (Timer timer) => setState(() {
+      oneSec,
+      (Timer timer) => setState(
+            () {
               if (_start < 1) {
                 timer.cancel();
+                _start = 5;
                 isTimer = false;
               } else {
                 _start = _start - 1;
                 isTimer = true;
               }
-            }));
+            },
+          ),
+    );
+  }
+
+  void startTimer2() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+            () {
+              if (_start2 < 1) {
+                timer.cancel();
+                _start2 = 10;
+                isTimer = false;
+              } else {
+                _start2 = _start2 - 1;
+                isTimer = true;
+              }
+            },
+          ),
+    );
+  }
+
+  void startTimer3() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+            () {
+              if (_start3 < 1) {
+                timer.cancel();
+                _start3 = 15;
+                isTimer = false;
+              } else {
+                _start3 = _start3 - 1;
+                isTimer = true;
+              }
+            },
+          ),
+    );
   }
 
   @override
@@ -50,18 +94,16 @@ class _bodyState extends State<BodyContent> {
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               RaisedButton(
-                child: Text('$_start' + ' MIN'),
-                onPressed: () => {
-                  startTimer()
-                },
+                child: Text('$_start' + ' SEC'),
+                onPressed: () => {startTimer()},
               ),
               RaisedButton(
-                child: Text('10 MIN'),
-                onPressed: () => {},
+                child: Text('$_start2' + ' SEC'),
+                onPressed: () => {startTimer2()},
               ),
               RaisedButton(
-                child: Text('15 MIN'),
-                onPressed: () => {},
+                child: Text('$_start3' + ' SEC'),
+                onPressed: () => {startTimer3()},
               ),
             ],
           ),
